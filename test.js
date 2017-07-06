6acaac22e66e73872b37e36e77d6607d7fb0c647
tshemsedinov <timur.shemsedinov@gmail.com>
2017-07-06 15:59:23 +0300

    Syntax fixes

--- test.js
+++ test.js
@@ -7,12 +7,14 @@ const data = {
   myFriend: {
     name: 'Marcus Aurelius',
     toString() {
-      return this.name
+      return this.name;
     }
   },
   positions: ['imperor', 'philosopher', 'writer']
 };
 
-const template1 = t`Example: ${'hello'} ${'myFriend'} great ${'positions'} of Rome`;
+const template1 = (
+  t`Example: ${'hello'} ${'myFriend'} great ${'positions'} of Rome`
+);
 
 console.log(template1(data));
