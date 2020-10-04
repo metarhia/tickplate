220a03c9a9f88f6f20f21f8f15b8d8eb726701c9
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-10-04 12:43:35 +0300

    Use for loop instead of forEach
    PR-URL: https://github.com/metarhia/tickplate/pull/31

--- tickplate.js
+++ tickplate.js
@@ -2,9 +2,10 @@
 
 const tickplate = (strings, ...keys) => values => {
   const result = [strings[0]];
-  keys.forEach((key, i) => {
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
     result.push(values[key], strings[i + 1]);
-  });
+  }
   return result.join('');
 };
 
