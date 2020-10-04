8125dbf6b64dd4663aafe3495e2bd9223278f664
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-10-04 12:43:05 +0300

    Remove debug output
    PR-URL: https://github.com/metarhia/tickplate/pull/31

--- tickplate.js
+++ tickplate.js
@@ -1,15 +1,11 @@
 'use strict';
 
-const tickplate = (strings, ...keys) => {
-  console.dir({ strings, keys });
-  return values => {
-    console.dir({ values });
-    const result = [strings[0]];
-    keys.forEach((key, i) => {
-      result.push(values[key], strings[i + 1]);
-    });
-    return result.join('');
-  };
+const tickplate = (strings, ...keys) => values => {
+  const result = [strings[0]];
+  keys.forEach((key, i) => {
+    result.push(values[key], strings[i + 1]);
+  });
+  return result.join('');
 };
 
 module.exports = tickplate;
