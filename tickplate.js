27838cbc992e7d668a300aa45d3c3f1285d279bd
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-12-15 23:59:52 +0200

    Refactor to support new linter rules
    PR-URL: https://github.com/metarhia/tickplate/pull/38

--- tickplate.js
+++ tickplate.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const tickplate = (strings, ...keys) => values => {
+const tickplate = (strings, ...keys) => (values) => {
   const result = [strings[0]];
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
