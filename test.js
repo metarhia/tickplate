61d1f5b469c551123f0031596a6509fdbf054691
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-12-16 02:54:57 +0200

    Improve tests

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const assert = require('assert').strict;
+
 const t = require('./tickplate.js');
 
 const data = {
@@ -13,6 +15,11 @@ const data = {
   positions: ['imperor', 'philosopher', 'writer'],
 };
 
-const templ = t`Example: ${'hello'} ${'myFriend'} great ${'positions'} of Rome`;
+const templ = t`${'hello'} ${'myFriend'}, great ${'positions'} of Rome`;
 
-console.log(templ(data));
+const expect = 'Ave! Marcus Aurelius, great imperor,philosopher,writer of Rome';
+const result = templ(data);
+assert.strictEqual(result, expect);
+assert.strictEqual(templ({ myFriend: 'Hadrian' }), ' Hadrian, great  of Rome');
+assert.strictEqual(templ({ hello: 'Hi!' }), 'Hi! , great  of Rome');
+assert.strictEqual(templ({}), ' , great  of Rome');
