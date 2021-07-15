b43d9b7e8e04ba9c09c4ffe4ae014ed2ef8ab00f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-07-15 22:00:35 +0300

    Apply new prettier style

--- tickplate.js
+++ tickplate.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const tickplate = (strings, ...keys) => (values) => {
-  const result = [strings[0]];
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    result.push(values[key], strings[i + 1]);
-  }
-  return result.join('');
-};
+const tickplate =
+  (strings, ...keys) =>
+  (values) => {
+    const result = [strings[0]];
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      result.push(values[key], strings[i + 1]);
+    }
+    return result.join('');
+  };
 
 module.exports = tickplate;
