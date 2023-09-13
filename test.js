e0a1f3ce953e83efab8a01305c8119761ac40979
Pavel <pavel.mikhalkevich@midge-medical.com>
2023-09-13 23:22:03 +0500

    Add support for default values

--- test.js
+++ test.js
@@ -4,22 +4,65 @@ const assert = require('node:assert').strict;
 
 const t = require('./tickplate.js');
 
-const data = {
-  hello: 'Ave!',
-  myFriend: {
-    name: 'Marcus Aurelius',
-    toString() {
-      return this.name;
+{
+  ({ description: 'No fallbacks used.' });
+
+  const data = {
+    hello: 'Ave!',
+    myFriend: {
+      name: 'Marcus Aurelius',
+      toString() {
+        return this.name;
+      },
     },
-  },
-  positions: ['imperor', 'philosopher', 'writer'],
-};
+    positions: ['emperor', 'philosopher', 'writer'],
+  };
 
-const templ = t`${'hello'} ${'myFriend'}, great ${'positions'} of Rome`;
+  const templ = t`${'hello'} ${'myFriend'}, great ${'positions'} of Rome`;
 
-const expect = 'Ave! Marcus Aurelius, great imperor,philosopher,writer of Rome';
-const result = templ(data);
-assert.strictEqual(result, expect);
-assert.strictEqual(templ({ myFriend: 'Hadrian' }), ' Hadrian, great  of Rome');
-assert.strictEqual(templ({ hello: 'Hi!' }), 'Hi! , great  of Rome');
-assert.strictEqual(templ({}), ' , great  of Rome');
+  const expect =
+    'Ave! Marcus Aurelius, great emperor,philosopher,writer of Rome';
+  const result = templ(data);
+  assert.strictEqual(result, expect);
+  assert.strictEqual(
+    templ({ myFriend: 'Hadrian' }),
+    ' Hadrian, great  of Rome',
+  );
+  assert.strictEqual(templ({ hello: 'Hi!' }), 'Hi! , great  of Rome');
+  assert.strictEqual(templ({}), ' , great  of Rome');
+}
+
+{
+  ({
+    description:
+      'Fallback primitives and arrays of primitives provided, with "=" separator, empty spaces around trimmed',
+  });
+
+  const data = {
+    greeting: 'Ave!',
+    person: {
+      name: 'Lucius Verus',
+      toString() {
+        return this.name;
+      },
+    },
+    positions: ['brother', 'emperor', 'co-emperor'],
+    ruleFrom: 161,
+    ruleTo: 169,
+  };
+
+  const templ = t`${'greeting='} ${'person = "Marcus Aurelius"'}, great ${'positions  =["emperor", "philosopher"]'} of Rome from ${'ruleFrom = 161'} to ${'ruleTo=180'} AD`;
+
+  const expect =
+    'Ave! Lucius Verus, great brother,emperor,co-emperor of Rome from 161 to 169 AD';
+  const result = templ(data);
+  assert.strictEqual(result, expect);
+  assert.strictEqual(
+    templ({}),
+    ' Marcus Aurelius, great emperor,philosopher of Rome from 161 to 180 AD',
+  );
+  assert.strictEqual(
+    templ({ greeting: 'Valē!!!', ruleFrom: '44 BC', ruleTo: 2023 }),
+    'Valē!!! Marcus Aurelius, great emperor,philosopher of Rome from 44 BC to 2023 AD',
+  );
+}
--- tickplate.js
+++ tickplate.js
@@ -1,12 +1,27 @@
 'use strict';
 
+const SEPARATOR = '=';
+
+const parseKeyValuePair = (value, sep = SEPARATOR) => {
+  const [lhs, ...rhs] = value.split(sep);
+  const key = lhs.trim();
+  if (rhs.length === 0) return { key };
+  const rhsRestored = rhs.join(sep).trim();
+  try {
+    const value = JSON.parse(rhsRestored);
+    return { key, value };
+  } catch {
+    return { key };
+  }
+};
+
 const tickplate =
   (strings, ...keys) =>
   (values) => {
     const result = [strings[0]];
     for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      result.push(values[key], strings[i + 1]);
+      const { key, value: fallback } = parseKeyValuePair(keys[i]);
+      result.push(values[key] || fallback, strings[i + 1]);
     }
     return result.join('');
   };
