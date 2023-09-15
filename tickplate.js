978756b00e7fecf74d0a6f5bac08c573fb7f965e
Pavel <pavel.mikhalkevich@midge-medical.com>
2023-09-15 13:04:49 +0500

    Add support for delimiter

--- tickplate.js
+++ tickplate.js
@@ -1,12 +1,19 @@
 'use strict';
 
+const serialize = (value, opts) => {
+  if (!Array.isArray(value)) return value;
+  const { delimiter } = opts;
+  return value.join(delimiter);
+};
+
 const tickplate =
   (strings, ...keys) =>
-  (values) => {
+  (values, opts = {}) => {
     const result = [strings[0]];
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
-      result.push(values[key], strings[i + 1]);
+      const value = serialize(values[key], opts);
+      result.push(value, strings[i + 1]);
     }
     return result.join('');
   };
